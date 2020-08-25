f0ff2b8b7501398f5c1818586c9bcb2c0a02f588
Orfeas Antoniou <orfeas@orfeasa.com>
2020-08-25 13:49:17 +0300

    Refactor day 1

--- Day 1 - Chronal Calibration/frequency_change_calc.py
+++ Day 1 - Chronal Calibration/frequency_change_calc.py
@@ -1,13 +1,18 @@
-if __name__ == "__main__":
-    input_file = "input.txt"
-    sum = 0
-    sums = set()
+from typing import List, Optional
+
+
+def read_input(input_file) -> List:
     frequency_changes = []
 
     with open(input_file) as f:
         for line in f.readlines():
             frequency_changes.append(int(line))
+    return frequency_changes
 
+
+def find_duplicate(frequency_changes) -> Optional[int]:
+    sums = set()
+    first_duplicate = None
     duplicate_found = False
     last_sum = 0
     while not (duplicate_found):
@@ -16,7 +21,15 @@ if __name__ == "__main__":
             if last_sum not in sums:
                 sums.add(last_sum)
             else:
-                print(f"First frequency repeated: {last_sum}")
-                print(f"Sums checked: {len(sums)}")
+                first_duplicate = last_sum
                 duplicate_found = True
                 break
+
+    return first_duplicate
+
+
+if __name__ == "__main__":
+    frequency_changes = read_input("input.txt")
+    duplicate = find_duplicate(frequency_changes)
+    if duplicate is not None:
+        print(f"First frequency repeated: {duplicate}")
